936f9d27bbcaa1a4470d5da4dcaab57d87267cfa
Hritambhar <hritambharray88594@gmail.com>
2024-07-01 14:16:24 +0530

    commit

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,7 +1,10 @@
+const { override } = require('customize-cra');
 const webpack = require('webpack');
-module.exports = function override(config) {
-  const fallback = config.resolve.fallback || {};
-  Object.assign(fallback, {
+
+module.exports = override((config) => {
+  // Define aliases for polyfills or specific modules
+  config.resolve.alias = {
+    ...config.resolve.alias,
     crypto: require.resolve('crypto-browserify'),
     stream: require.resolve('stream-browserify'),
     assert: require.resolve('assert'),
@@ -9,13 +12,15 @@ module.exports = function override(config) {
     https: require.resolve('https-browserify'),
     os: require.resolve('os-browserify'),
     url: require.resolve('url'),
-  });
-  config.resolve.fallback = fallback;
-  config.plugins = (config.plugins || []).concat([
+  };
+
+  // Add webpack plugins such as ProvidePlugin
+  config.plugins.push(
     new webpack.ProvidePlugin({
       process: 'process/browser',
       Buffer: ['buffer', 'Buffer'],
-    }),
-  ]);
+    })
+  );
+
   return config;
-};
+});
